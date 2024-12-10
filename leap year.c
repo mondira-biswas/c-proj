1a0f5a1fcffe30982f01e80b1997321bc9b950fd
Mondira Biswas <biswasmondira57@gmail.com>
2024-12-10 12:47:45 +0600

    Add files via upload

---
+++ leap year.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int year;
+printf("Enter year:");
+scanf("%d",&year);
+if(year%400==0)
+printf("leap year:");
+else if(year%4==0 && year%100!=0)
+printf("leap year:");
+else
+printf("not leap year:");
+return 0;
+
+}

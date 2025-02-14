3a7c243cf3461241faad8de4977fcd80d2f9ee06
Mondira Biswas <biswasmondira57@gmail.com>
2025-02-14 07:34:41 +0600

    Add files via upload

---
+++ ReverseCharacter.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    char char1='X';
+    char char2='M';
+    char char3='L';
+    printf("%c%c%C is a character=%C%C%C",char1,char2,char3,char3,char2,char1);
+    return 0;
+}

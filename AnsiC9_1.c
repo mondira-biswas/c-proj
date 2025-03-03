27f941b6fc116a3a73ea8cf46053eaf993563563
Mondira Biswas <biswasmondira57@gmail.com>
2025-03-03 18:43:54 +0600

    Add files via upload

---
+++ AnsiC9_1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    char word1[40],word2[40],word3[40],word4[40];
+    printf("Enter text=\n");
+    scanf("%s %s %s %s",word1,word2,word3,word4);
+    printf("\n");
+    printf("word1=%s\n word2=%s\n word3=%s\n word4=%s\n",word1,word2,word3,word4);
+    return 0;
+}

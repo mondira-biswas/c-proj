ad55032ede31cff4be23aa7e9d07f440aaa82c79
Mondira Biswas <biswasmondira57@gmail.com>
2025-03-12 08:44:45 +0600

    Add files via upload

---
+++ FirstDigit.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+    scanf("%d",&x);
+    while(x>=10)
+    {
+        x=x/10;
+    }
+    if(x%2==0)
+    {
+        printf("EVEN\n");
+    }
+    else{
+        printf("ODD\n");
+    }
+    return 0;
+}

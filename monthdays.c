7c2b61313d30cddfc956ceb9ae32e61067fabfbe
Mondira Biswas <biswasmondira57@gmail.com>
2025-03-12 08:46:08 +0600

    Add files via upload

---
+++ monthdays.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+    scanf("%d",&x);
+    switch(x)
+    {
+        case 1:
+        printf("Month has 31 days");
+        break;
+        case 2:
+        printf("Month has 28 days");
+        break;
+        case 3:
+        printf("Month has 31 days");
+        break;
+        case 4:
+        printf("Month has 30 days");
+        break;
+        case 5:
+        printf("Month has 31 days");
+        break;
+        case 6:
+        printf("Month has 30 days");
+        break;
+        case 7:
+        printf("Month has 31 days");
+        break;
+        case 8:
+        printf("Month has 31 days");
+        break;
+        case 9:
+        printf("Month has 30 days");
+        break;
+        case 10:
+        printf("Month has 31 days");
+        break;
+        case 11:
+        printf("Month has 30 days");
+        break;
+        case 12:
+        printf("Month has 31 days");
+        break;
+        default:
+        printf("Invaild month");
+        break;
+    
+        
+
+    }
+}

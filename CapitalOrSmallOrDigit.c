39db206c65348060c7516c7ac1aacd7965615d10
Mondira Biswas <biswasmondira57@gmail.com>
2025-03-12 08:41:38 +0600

    Add files via upload

---
+++ CapitalOrSmallOrDigit.c
@@ -0,0 +1,33 @@
+
+#include<stdio.h>
+
+int main()
+{
+    char X;
+
+
+    scanf("%c", &X);
+
+    
+    if (X >= '0' && X <= '9')
+    {
+        printf("IS DIGIT\n");
+    }
+    
+    else if ((X >= 'A' && X <= 'Z') || (X >= 'a' && X <= 'z'))
+    {
+        printf("ALPHA\n");
+
+        
+        if (X >= 'A' && X <= 'Z')
+        {
+            printf("IS CAPITAL\n");
+        }
+        else
+        {
+            printf("IS SMALL\n");
+        }
+    }
+
+    return 0;
+}

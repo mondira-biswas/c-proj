b27ed2817be7e8b7eb69e38d09ff0876cfcfe7cf
Mondira Biswas <biswasmondira57@gmail.com>
2025-03-12 08:39:44 +0600

    Add files via upload

---
+++ SmipleCalculator.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int X, Y;
+    scanf("%d %d", &X, &Y);
+    printf("%d + %d = %d\n", X, Y, X + Y );
+    printf("%d * %d = %d\n", X, Y, X * Y);
+    printf("%d - %d = %d\n", X, Y, X - Y);
+    return 0;
+}

1026ca9c187844ccb30fbec6e10017c661023f0c
Mondira Biswas <biswasmondira57@gmail.com>
2025-03-12 08:42:27 +0600

    Add files via upload

---
+++ Char.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    char x;
+    scanf("%c",&x);
+    if(x>='a' && x<='z')
+    printf("%c\n",x-32);
+    else if(x>='A' && x<='Z')
+    printf("%c\n",x+32);
+    return 0;
+
+}

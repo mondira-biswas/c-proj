c22833e1f9b3b5e36a85226cf71e441205c75aeb
Mondira Biswas <biswasmondira57@gmail.com>
2025-03-13 22:20:33 +0600

    Add files via upload

---
+++ pattern26.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+int main()
+{
+    int n, row, col;
+    scanf("%d", &n);
+    for (row = 1; row <= n; row++)
+    {
+        // printing space
+        for (col = 1; col <= n - row; col++)
+        {
+            printf(" ");
+        }
+        for (col = 1; col <= row; col++)
+        {
+            printf("%d", col);
+        }
+        for (col = row - 1; col >= 1; col--)
+        {
+            printf("%d", col);
+        }
+        printf("\n");
+    }
+    for (row = n-1; row >= 1; row--)
+    {
+        // printing space
+        for (col = 1; col <= n - row; col++)
+        {
+            printf(" ");
+        }
+        for (col = 1; col <= row; col++)
+        {
+            printf("%d", col);
+        }
+        for (col = row - 1; col >= 1; col--)
+        {
+            printf("%d", col);
+        }
+        printf("\n");
+    }
+    return 0;
+}
